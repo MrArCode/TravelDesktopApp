9b49f6155782bde5f07b5e97c90a25c63d7b6048
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-12-30 16:53:26 +0100

    Adding new gui

--- src/main/java/org/example/traveldesktopapp/view/App.java
+++ src/main/java/org/example/traveldesktopapp/view/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/main-panel-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/main-panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
