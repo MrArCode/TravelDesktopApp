d71b4c7ec6d9b91dc3006dc6a7ef40b345e2cefd
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-12-30 20:37:42 +0100

    Adding one more button and logic to load the files

--- src/main/java/org/example/traveldesktopapp/view/StartPanel.java
+++ src/main/java/org/example/traveldesktopapp/view/StartPanel.java
@@ -1,10 +1,18 @@
 package org.example.traveldesktopapp.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Popup;
+
+import java.io.File;
+import java.util.List;
 
 public class StartPanel {
 
@@ -15,7 +23,8 @@ public class StartPanel {
     private ImageView homeButton;
 
     @FXML
-    private ListView<?> listOfFiles;
+    private ListView<File> listOfFiles;
+    private ObservableList<File> files;
 
     @FXML
     private ImageView searchButton;
@@ -27,26 +36,53 @@ public class StartPanel {
     private ImageView trashButton;
 
     @FXML
-    void addOnMouseClicked(MouseEvent event) {
-        System.out.println("tak");
+    private ImageView uploadButton;
 
+    @FXML
+    public void initialize() {
+        files = FXCollections.observableArrayList();
+        listOfFiles.setItems(files);
+
+        listOfFiles.setCellFactory(_ -> new ListCell<>() {
+            @Override
+            protected void updateItem(File file, boolean empty) {
+                super.updateItem(file, empty);
+                if (empty || file == null) {
+                    setText(null);
+                } else {
+                    setText(file.getName());
+                }
+            }
+        });
+    }
+
+    @FXML
+    void addOnMouseClicked(MouseEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files","*.txt"));
+        List<File> chosenFiles = fileChooser.showOpenMultipleDialog(null);
+        if (chosenFiles != null) {
+            files.addAll(chosenFiles);
+        }
     }
 
     @FXML
     void homeOnMouseClicked(MouseEvent event) {
-        System.out.println("tak");
+        System.out.println("Home clicked!");
     }
 
     @FXML
     void searchOnMouseClicked(MouseEvent event) {
-        System.out.println("tak");
+        System.out.println("Search clicked!");
     }
 
     @FXML
     void trashOnMouseClicked(MouseEvent event) {
-        System.out.println("tak");
+        System.out.println("Trash clicked!");
     }
 
-
-
+    @FXML
+    void uploadOnMouseClicked(MouseEvent event) {
+        System.out.println("Upload clicked!");
+    }
 }
