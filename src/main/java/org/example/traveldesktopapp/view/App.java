7ccc42f3c562447bbb12ae63d3aea520554d32f5
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2025-01-04 20:12:21 +0100

    Title change

--- src/main/java/org/example/traveldesktopapp/view/App.java
+++ src/main/java/org/example/traveldesktopapp/view/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/main-panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Travel App");
         stage.setScene(scene);
         stage.show();
     }
